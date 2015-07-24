59673f5d5702bc6fbbb05f5b342922cf14c2ea3d
Christian Pfitzner <you@example.com>
2015-07-24 12:54:01 +0200

    -- added test script

--- octave/ExtractFeatures.m
+++ octave/ExtractFeatures.m
@@ -3,13 +3,25 @@
 %% output parameter: 
 %          f: output vector containing features
 function [f] = maxmin(W )
-   if(a >= b )
-        max = a;
-        min = b;
-        return;
-   else
-        max = b;
-        min = a;
-        return;
-   endif
+
+  % check for dimension
+  n = length(W')
+
+   % calculate pca
+   
+   n = length(W)
+
+   % calculate features from eigen values
+   eig = [1.0, 1.0]; 
+   
+   % linearity
+   f(1) = eig(1) / (eig(1) + eig(2));
+   
+   % circularity 
+   f(2) = eig(2) / (eig(1) + eig(2)); 
+
+   % standard deviation from centroid 
+   c = mean(W); 
+   f(3) = sqrt(1/2); 
+
 endfunction
---
+++ octave/extract_features_test.m
@@ -0,0 +1,8 @@
+%% extract features test
+
+P = [1, 1; 
+        2, 2; 
+        3, 3]
+        
+        
+f = ExtractFeatures(P)
