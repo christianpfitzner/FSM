fc7f6b620f97c12f91343f5c86039eb5a253cd21
Christian Pfitzner <you@example.com>
2015-07-24 11:56:38 +0200

    -- initial commit for feature extraction function

---
+++ octave/ExtractFeatures.m
@@ -0,0 +1,15 @@
+%% input parameter: 
+%        W: input matrix containing points for feature extraction
+%% output parameter: 
+%          f: output vector containing features
+function [f] = maxmin(W )
+   if(a >= b )
+        max = a;
+        min = b;
+        return;
+   else
+        max = b;
+        min = a;
+        return;
+   endif
+endfunction
